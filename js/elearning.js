b1077f63ed965274724e12b31060fe1096014d35
hungMta <hung250596@gmail.com>
2017-09-14 00:45:06 +0700

    add trash

--- js/elearning.js
+++ js/elearning.js
@@ -2,6 +2,7 @@ $(document).on('ready', function() {
     $(".lazy").slick({
       lazyLoad: 'ondemand', // ondemand progressive anticipated
       infinite: true,
-      autoplay: true
+      autoplay: true,
+      autoplaySpeed: 2000,
     });
   });
